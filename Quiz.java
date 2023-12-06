aa4a0befaa18f76384e066c1de38e0c7ab29a560
farelvy <farelhajiri2@gmail.com>
2023-12-06 20:50:29 +0700

    modifikasi

--- Quiz.java
+++ Quiz.java
@@ -4,19 +4,42 @@ public class Quiz {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Random rand = new Random();
-        char menu = 'y';
+
+        char menu = 'Y';
         do {
             int number = rand.nextInt(10) + 1;
             boolean success = false;
+
             do {
                 System.out.print("Tebak angka (1-10): ");
                 int answer = input.nextInt();
                 input.nextLine();
-                success= (answer == number);
+                success = (answer == number);
+
+                if (success) {
+                    System.out.println("Selamat, Anda benar!");
+                } else {
+                    System.out.println("Coba lagi.");
+                    if (answer < number){
+                        System.out.println("Angka yang di masukkan lebih kecil dari jawaban");
+                    } else {
+                        System.out.println("Angka lebih besar dari jawaaban");
+                    }
+                }
+
             } while (!success);
-            System.out.print("Apakah anda ingin mengulang permainan (Y/T)");
+
+            System.out.print("Apakah anda ingin mengulang permainan (Y/T)? ");
             menu = input.next().charAt(0);
             input.nextLine();
-        } while (menu  == 'Y' || menu == 'y');
+
+            if (menu != 'Y' && menu != 'y') {
+                break; 
+            }
+
+        } while (menu == 'Y' || menu == 'T');
+
+        System.out.println("Terima kasih telah bermain. Sampai jumpa!");
     }
 }
+
